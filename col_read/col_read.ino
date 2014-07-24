981b8f9ebe8624aa654e49559129b0106c8d655e
David Selassie <dselassi@yelp.com>
2014-07-24 16:58:21 -0700

    Adds in servo control code.

--- col_read/col_read.ino
+++ col_read/col_read.ino
@@ -1,16 +1,33 @@
+#include <Servo.h>
+
 // Sensor config.
-const int numSensors = 1;
-int sensorPins[numSensors] = {0};
+const int numSensors = 2;
+const int sensorPins[numSensors] = {
+  0, 15};
+
+// Servo config.
+const int colSelectorServoPin = 51;
+const int colDegree = 10;
+const int dispenserServoPin = 49;
 
 // Detection parameters.
-int checkLoopMS = 1;
-int debounceDelayMS = 50;
-int detectionThreshold = 865;
+const int checkLoopMS = 1;
+const int debounceDelayMS = 50;
+const int detectionThreshold = 865;
+
+// Ramp physics parameters.
+const int rampRotateMS = 1000;
+const int coinRollMS = 1000;
+const int dispenseMS = 1000;
 
 // Private state for debouncing.
 int lastReading[numSensors];
 int lastConfirmedState[numSensors];
 long lastReadingChangeMS[numSensors];
+
+// Private state for ramp servo.
+Servo colSelectorServo;
+Servo dispenserServo;
 
 void setup() {
   // Zero debounce state.
@@ -19,24 +36,54 @@ void setup() {
     lastConfirmedState[i] = 0;
     lastReadingChangeMS[i] = 0L;
   }
-  
+
+  colSelectorServo.attach(colSelectorServoPin);
+  colSelectorServo.write(0);
+  dispenserServo.attach(dispenserServoPin);
+  dispenserServo.write(0);
+
   Serial.begin(9600);
 }
 
 void loop() {
+  commandRead();
+  boardRead();
+  delay(checkLoopMS);
+}
+
+void commandRead() {
+  if (Serial.available()) {
+    int inCol = Serial.read() - 48;
+    if (inCol >= 0 && inCol <= 6) {
+      dispenseInCol(inCol);
+    }
+  }
+}
+
+void dispenseInCol(int col) {
+    colSelectorServo.write((col + 1) * colDegree);
+    delay(rampRotateMS);
+    dispenserServo.write(90);
+    delay(dispenseMS);
+    delay(coinRollMS);
+    dispenserServo.write(0);
+    colSelectorServo.write(0);
+    delay(rampRotateMS);
+}
+
+void boardRead() {
   for (int i = 0; i < numSensors; i++) {
     int sensorPin = sensorPins[i];
     int reading = getReading(sensorPin);
     int newState = confirmState(i, reading, lastConfirmedState[i]);
     if(newState != lastConfirmedState[i]) {
       lastConfirmedState[i] = newState;
-      
+
       if (newState == 0) {
         Serial.println(i);
       }
     }
   }
-  delay(checkLoopMS);
 }
 
 int confirmState(int i, int reading, int lastConfirmedState) {
